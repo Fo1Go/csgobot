5855bcbfe686e6bfe21c1375cec9e83f268e4f69
Usevalad Prastsiakou <folgo1006@gmail.com>
2024-04-23 16:11:58 +0300

    README.md file changed. Added env variables

---
+++ README.md
@@ -0,0 +1,8 @@
+python 3.11.7\
+aiogram\
+sqlAlchemy\
+alembic
+
+compose file runs postgreSQL\
+run.py runs bot at @Counter_Strike_news_bot\
+create_tables create tables at postgreSQL
--- bot/manager.py
+++ bot/manager.py
@@ -1,12 +1,16 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from .main.routers import router as main_router
+import os
+from dotenv import load_dotenv
+
+load_dotenv("env_file")
 
 
 class BotManager:
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
-        self.token = '1409312853:AAFkRNCoLO_LbCmJDPVfHrQCqAaCMLoosl8'
+        self.token = os.getenv("CSGOBOT_TOKEN")
         self.bot = Bot(token=self.token)
         self.dp = Dispatcher()
 
